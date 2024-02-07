d12b3a7394386258822cbf52e27ae25bcf0de929
Trey Orr <treyorr20@gmail.com>
2024-02-07 14:04:54 -0500

    3d

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,39 @@
+"use client";
+
+import { Model } from "@/components/Microscope/Microscope_swift_sw380b";
+import { Canvas } from "@react-three/fiber";
+
 export default function Home() {
-    return <button className='btn btn-primary'>Hello trlr!</button>;
+    return (
+        <div className='flex flex-col md:flex-row'>
+            <div className='md:w-1/2 md:pr-4 pl-10 pt-10'>
+                <div class='chat chat-start'>
+                    <div class='chat-bubble chat-bubble-info'>
+                        <div class='text-white font-semibold text-2xl'>
+                            Hi, I&apos;m Hayley.
+                        </div>
+                    </div>
+                </div>
+                <div class='chat chat-start pt-4'>
+                    <div class='chat-bubble chat-bubble-info'>
+                        <div class='text-white font-semibold text-2xl'>
+                            I&apos;m an aspiring forensic pathologist.
+                        </div>
+                    </div>
+                </div>
+                <div class='chat chat-start pt-4'>
+                    <div class='chat-bubble chat-bubble-info'>
+                        <div class='text-white font-semibold text-2xl'>
+                            Welcome to my personal website.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='md:w-1/2 md:pl-4'>
+                <Canvas>
+                    <Model />
+                </Canvas>
+            </div>
+        </div>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui'
+import daisyui from "daisyui";
 module.exports = {
-  mode: "jit",
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#fb7185",
+                    secondary: "#fbcfe8",
+                    accent: "#9f1239",
+                    neutral: "#ffffff",
+                    "base-100": "#fecdd3",
+                    info: "#f472b6",
+                    success: "#e11d48",
+                    warning: "#ffffff",
+                    error: "#ffffff",
+                },
+            },
+        ],
     },
-  },
-  plugins: [daisyui],
+    mode: "jit",
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
+    },
+    plugins: [daisyui],
 };
