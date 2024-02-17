a1198c981eae20bedab8d3bca330197e953a8298
Trey Orr <treyorr20@gmail.com>
2024-02-17 17:17:12 -0500

    fix

--- src/app/awards/page.js
+++ src/app/awards/page.js
@@ -3,7 +3,7 @@ export default function Awards() {
         <div className='grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-4'>
             <div className='w-full 2xl:w-2/3 md:p-4 pt-4 px-2'>
                 <div className='block max-w-sm p-6 bg-primary rounded-lg text-center mt-4'>
-                    <div className='text-xl font-bold'>President's List</div>
+                    <div className='text-xl font-bold'>President&apos;s List</div>
                     <div className='text-sm font-normal'>Daytona State College</div>
                     <div className='text-sm italic mt-1'>
                         Recognition for achieving a 4.0 semester GPA
@@ -19,7 +19,7 @@ export default function Awards() {
                     </div>
                 </div>
                 <div className='block max-w-sm p-6 bg-primary rounded-lg text-center mt-6'>
-                    <div className='text-xl font-bold'>Dean's List</div>
+                    <div className='text-xl font-bold'>Dean&apos;s List</div>
                     <div className='text-sm font-normal'>Daytona State College</div>
                     <div className='text-sm italic mt-1'>
                         Recognition for achieving above a 3.75 semester GPA
